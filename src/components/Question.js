bb04a7bd8231f2f899d000deeb5a77806ab3163d
Muny1re1 <munyiriandy@gmail.com>
2024-05-03 17:53:21 +0300

    useEffect

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  // side effect for the setTimeout that decreases the time by 1 every second and its clean up function.
+  useEffect(()=>{
+    const timer = setTimeout(()=>{
+      setTimeRemaining(currentTime => currentTime -1)
+    }, 1000);
+    return ()=>{
+      clearTimeout(timer);
+    };
+    //needed variables.
+  }, [timeRemaining]
+  );
+
+  // side effect for calling the prop onAnswer(false) and resetting the time back to 10 when it reaches 0.
+  useEffect(()=>{
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
